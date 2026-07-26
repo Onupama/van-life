d7d9c844fa892aa22bbdea87eb4dea798f35f6b1
Anupama Ghosh <onupama.s@gmail.com>
2026-07-26 19:31:59 +0530

    Update Vans.jsx

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -4,7 +4,7 @@ export default function Vans() {
     const [vans, setVans] = React.useState([])
 
     React.useEffect(() => {
-        fetch("/api/avans")
+        fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
